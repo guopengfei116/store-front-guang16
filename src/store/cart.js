69831a2feea886879d1661b789661fb44eb9522b
PengFei <626138959@qq.com>
2018-03-08 11:42:43 +0800

    物车数据持久化

--- src/store/cart.js
+++ src/store/cart.js
@@ -2,7 +2,9 @@ export default {
 
     // 定义状态, 相当于data数据
     state: {
-        cart: {} // 数据结构预览: { 153: 5, 154: 10, 155: 12 }, ID为key, 数量为value
+        // 这里的初始数据优先从storage里获取, 如果没有就设一个空对象
+        // 数据结构预览: { 153: 5, 154: 10, 155: 12 }, ID为key, 数量为value
+        cart: JSON.parse(localStorage.getItem('cart')) || {} 
     },
 
     // 定义修改状态的方法, 这里的方法需要通过store.commit(方法名称, 参数)
@@ -13,6 +15,8 @@ export default {
             let { id, num } = data; // es6的语法, 解构里面的id与num值
             state.cart[id] = num;   // 根据id, 修改对应的购买数量值
 
+            // 每次状态变化后, 把新的状态记录在本地storage里, 放置页面刷新后数据丢失
+            localStorage.setItem('cart', JSON.stringify(state.cart));
             console.log(state.cart);
         }
     }
