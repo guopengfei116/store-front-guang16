b5f6efb2d422a708ca87a42378e4476874212001
PengFei <626138959@qq.com>
2018-03-08 17:05:57 +0800

    添加登陆校验

--- src/router/index.js
+++ src/router/index.js
@@ -25,12 +25,12 @@ let shopcart = [
 ];
 
 let order = [ 
-  { name: 'orderSite', path: 'order/site/:ids', component: OrderSite }, // 需要知道哪些商品要下单
+  { name: 'orderSite', path: 'order/site', component: OrderSite }, // 需要知道哪些商品要下单
   { name: 'orderPay', path: 'order/pay/:id', component: OrderPay }, // 需要知道订单ID才能付款
   { name: 'orderComplete', path: 'order/complete', component: OrderComplete }
 ];
 
-export default new Router({
+let router = new Router({
   routes: [
 
     // 账户管理路由配置
@@ -39,4 +39,59 @@ export default new Router({
     // 商城路由配置
     { name: 'shop', path: '/', component: Shop, children: [ ...goods, ...shopcart, ...order ] },
   ]
-})
+});
+
+// 网站白名单, 加到这里页面都不用校验登陆权限
+let baidan = ['goodsList', 'goodsDetail', 'shopcart'];
+
+// 添加路由前置守卫, 其实就是添加一个函数, 这个函数会在路由配置之前执行, 我们可以在这里添加一些登陆或者权限的校验
+// to与from是两个对象, 可以拿到url信息
+router.beforeEach((to, from, next) => {
+  // 通过to.name得知用户访问的是什么页面, 如果是admin相关页面, 那么继续判断用户有没有登陆
+  // 有 => 调用next()通过访问,  没有 => 调用next({name: 'login'})跳转到登陆页面
+
+  // 判断当前访问的页面在不在白名单中
+  let isTrue = baidan.some(v => v == to.name);
+
+  // 如果访问的是白名单页面, 直接通过, 终止下面代码的执行
+  if(isTrue) {
+    next();
+    return;
+  }
+  
+  // 现在我们是在一个js模块当中, 并不是在vue组件中, 所以这里的this不是组件实例, 而是undefined, 无法this.$http与this.$api
+  Vue.prototype.$http.get(Vue.prototype.$api.isLogin).then(res => {
+      let isLogin = false;
+
+      // 已登陆
+      if(res.data.code == 'logined') {
+          isLogin = true;
+      }
+
+      // 如果访问登陆页面
+      // 已登陆 => 为了用户友好体验, 自动跳转到后台管理
+      // 未登陆 => 允许访问登陆页面
+      if(to.name == 'login') {
+          if(isLogin) {
+              next({name: 'goodsList'});
+          }else {
+              next();
+          }
+      }
+
+      // 如果访问后台页面
+      // 已登陆 => 允许访问后台页面 
+      // 未登陆 => 禁止访问, 自动跳转到登陆页面
+      if(to.name != 'login') {
+          if(isLogin) {
+              next();
+          }else {
+              // query用来设置url中的查询字符串, 我们这里把用户要访问的页面地址通过query记录下来
+              // 将来用户登陆成功后, 再自动跳回这个地址
+              next({name: 'login', query: {next: to.fullPath}}); 
+          }
+      }
+  })
+});
+
+export default router;
